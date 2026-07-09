92085ad67d788f8a8ba54acc6d72ab29e79523fe
lokeshwaran09 <24243051@drngpit.ac.in>
2026-07-09 15:10:30 +0530

    Create 405-ConvertANumberToHexadecimal

---
+++ 405-ConvertANumberToHexadecimal/405-ConvertANumberToHexadecimal.java
@@ -0,0 +1,14 @@
+// Last updated: 7/9/2026, 3:10:30 PM
+class Solution {
+    public String toHex(int num) {
+        if(num == 0) return "0";
+        StringBuilder str = new StringBuilder();
+        char[] hexChars = "0123456789abcdef".toCharArray();
+        while(num != 0){
+            int rem = num & 15;
+            str.append(hexChars[rem]);
+            num >>>=4;
+        }
+        return str.reverse().toString();
+    }
+}
