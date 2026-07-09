918be2a150348286d8e32ea43d063895b0e33279
lokeshwaran09 <24243051@drngpit.ac.in>
2026-07-09 15:13:09 +0530

    Create 51-NQueens

---
+++ 51-NQueens/51-NQueens.java
@@ -0,0 +1,55 @@
+// Last updated: 7/9/2026, 3:13:10 PM
+class Solution {
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> result = new ArrayList<>();
+        char[][] board = new char[n][n];
+
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(board[i], '.');
+        }
+
+        backtrack(board, 0, result);
+        return result;
+    }
+
+    private void backtrack(char[][] board, int row, List<List<String>> result) {
+        if (row == board.length) {
+            List<String> list = new ArrayList<>();
+            for (char[] r : board) {
+                list.add(new String(r));
+            }
+            result.add(list);
+            return;
+        }
+
+        for (int col = 0; col < board.length; col++) {
+            if (isSafe(board, row, col)) {
+                board[row][col] = 'Q';
+                backtrack(board, row + 1, result);
+                board[row][col] = '.';
+            }
+        }
+    }
+
+    private boolean isSafe(char[][] board, int row, int col) {
+        for (int i = 0; i < row; i++) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
